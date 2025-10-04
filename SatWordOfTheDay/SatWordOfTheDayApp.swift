b217b7f78569e09cc4fc6eca3c2aefbc25792e3a
aaron <aaronkramp@live.com>
2025-10-03 17:37:41 -0700

    Initial Commit

---
+++ SatWordOfTheDay/SatWordOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatWordOfTheDayApp.swift
+//  SatWordOfTheDay
+//
+//  Created by akramp on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SatWordOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
